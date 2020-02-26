a0feed0f489d92eeb6406a72c9fe1f7ac03e8a91
Alexander Herrmann <alexander.herrmann@freenet.de>
2020-02-26 15:28:54 +0100

    Beschriftung in m-Datei

--- MATLAB_Simulink/Variablen_ProjektB.m
+++ MATLAB_Simulink/Variablen_ProjektB.m
@@ -1,17 +1,30 @@
+% Massen
 m1 = 90.;
 m2 = 10.;
+% Federkonstante
 c = 1600.;
+% Daempfungskonstanten
 dt = 5.;
 dr = 10.;
+% Stablaenge
 e = 0.2;
+% Ankerflussverkettung
 K_A = 50.;
+% Ohmscher Widerstand
 R_A = 0.1;
+% Induktiver Widerstand
 L_A = 10.;
+% Klemmenspannung
 U = 100.;
+% Erdbeschleunigung
 g = 9.81;
 
+% Simulation
 sim('Block_Diagramm_ProjektB')
+% Scopes finden
 scopes = find_system(gcs, 'BlockType', 'Scope');
+% Alle Scopes anzeigen
 for i=1 : length(scopes)
 set_param(scopes{i},'open','on');
 end
+
