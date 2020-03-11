046b29328bbce74ea6b57d1ca6775d02e020530e
Alexander Herrmann <alexander.herrmann@freenet.de>
2020-03-11 14:07:33 +0100

    110320

--- MATLAB_Simulink/Variablen_ProjektB.m
+++ MATLAB_Simulink/Variablen_ProjektB.m
@@ -4,8 +4,9 @@ m2 = 10.;
 % Federkonstante
 c = 1600.;
 % Daempfungskonstanten
-dt = 5.;
-dr = 10.;
+dt = 1.;
+%dr = 10.;
+dr = 0.01;
 % Stablaenge
 e = 0.2;
 % Ankerflussverkettung
@@ -15,16 +16,24 @@ R_A = 0.1;
 % Induktiver Widerstand
 L_A = 10.;
 % Klemmenspannung
+%U = 100.;
 U = 100.;
 % Erdbeschleunigung
 g = 9.81;
 
 % Simulation
-sim('Block_Diagramm_ProjektB')
-% Scopes finden
-scopes = find_system(gcs, 'BlockType', 'Scope');
-% Alle Scopes anzeigen
-for i=1 : length(scopes)
-set_param(scopes{i},'open','on');
-end
+%sim('Block_Diagramm_ProjektB')
 
+plot(out.s_out);
+xlabel('Zeit in s');
+ylabel('Weg in m');
+title('Auslenkung');
+
+plot(out.s_out*1000);
+hold on;
+%plot(out.F_U_out);
+%hold off;
+xlabel('Zeit in s');
+ylabel('Weg s_{out} * 1000 in m, UnwuchtkraftF_U in N', 'Interpreter', 'tex');
+title('Auslenkung und Unwuchtkraft');
+legend('Weg s_{out} * 1000', 'UnwuchtkraftF_U');
